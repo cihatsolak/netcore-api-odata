594529a08780d287fb9e0f30da9fab8cda7cdce4
Cihat Solak <cihatsolak@hotmail.com>
2021-02-02 14:30:13 +0300

    database created with migration

---
+++ NetCoreAPIOData/OData.API/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OData.API.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
