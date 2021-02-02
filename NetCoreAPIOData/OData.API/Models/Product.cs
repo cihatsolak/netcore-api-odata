dee1e0b82bdf0b07b38f32e57f6998d31596d7ec
Cihat Solak <cihatsolak@hotmail.com>
2021-02-02 13:39:14 +0300

    entities were created.

---
+++ NetCoreAPIOData/OData.API/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OData.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public int Price { get; set; }
+        public DateTime? CreatedOn { get; set; }
+
+        //[ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
