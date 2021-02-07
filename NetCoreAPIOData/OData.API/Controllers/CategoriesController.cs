90784c0e3fd8ab2a5314e4d6da273f2523badb10
Cihat Solak <cihatsolak@hotmail.com>
2021-02-07 19:26:29 +0300

    Action and function methods.

--- NetCoreAPIOData/OData.API/Controllers/CategoriesController.cs
+++ NetCoreAPIOData/OData.API/Controllers/CategoriesController.cs
@@ -51,5 +51,46 @@ namespace OData.API.Controllers
         {
             return Ok(_dbContext.Products.Where(p => p.CategoryId == categoryId && p.Id == productId));
         }
+
+        /// <summary>
+        /// Custom Path (Parametresiz Action-> Body)
+        /// </summary>
+        /// <param name="key">Category Id</param>
+        /// <returns>Toplam Ürün Fiyatı</returns>
+        [HttpPost]
+        public IActionResult TotalProductPrice([FromODataUri] int key) //key: odata
+        {
+            var total = _dbContext.Products.Where(p => p.CategoryId == key).Sum(p => p.Price);
+
+            return Ok($"Total: {total}");
+        }
+
+        /// <summary>
+        /// Custom Path (Parametreli Action-> Body)
+        /// </summary>
+        /// <param name="parameters">parameters</param>
+        /// <returns>Toplam Ürün Fiyatı</returns>
+        [HttpPost]
+        public IActionResult TotalProductPriceWithParameter(ODataActionParameters parameters)
+        {
+            int categoryId = (int)parameters["categoryId"];
+            int minimumStock = (int)parameters["minimumStock"];
+
+            var total = _dbContext.Products.Where(p => p.CategoryId == categoryId && p.Stock >= minimumStock).Sum(p => p.Price);
+
+            return Ok($"Total: {total}");
+        }
+
+        /// <summary>
+        /// Custom Path (Uri parametresiz)
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult CategoryCount()
+        {
+            int count = _dbContext.Categories.Count();
+
+            return Ok($"Category Count: {count}");
+        }
     }
 }
---
+++ NetCoreAPIOData/OData.API/Models/DataTransferObjects/LoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace OData.API.Models.DataTransferObjects
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
